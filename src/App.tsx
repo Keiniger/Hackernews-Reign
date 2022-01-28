6d35d5c3ce7adb2d17d6d84a026a180f12a1d011
Nacho <nkeiniger@gmail.com>
2022-01-28 14:23:42 -0300

    change selection color

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,16 @@ import { getFromLocalStorage, Keys } from "./utils/localStorage";
 import Header from "./components/Header";
 import FilterFramework from "./components/FilterFramework";
 import FilterFaves from "./components/FilterFaves";
-import { FilterType } from "./components/FilterFaves";
 //import NewsList from "./components/News/NewsList";
 import Paginator from "./components/Paginator";
 import styles from "./App.module.css";
 
 export default function App() {
-  const defaultFramework = "React";
-  const frameworkFromLocalStorage = getFromLocalStorage(
-    Keys.Framework
-  );
-  const [selectedFramework, setSelectedFramework] = useState<string>(
-    frameworkFromLocalStorage || defaultFramework
-  );
-
+  const frameworkFromLocalStorage = getFromLocalStorage(Keys.Framework);
   const filterFromLocalStorage = getFromLocalStorage(Keys.Filter);
-  console.log("from storage favs filter: " + filterFromLocalStorage);
-  const defaultFilter = filterFromLocalStorage || FilterType.All;
-  const [selectedFilter, setSelectedFilter] = useState<string>(defaultFilter);
+
+  const [selectedFramework, setSelectedFramework] = useState(frameworkFromLocalStorage || "React")
+  const [selectedFilter, setSelectedFilter] = useState(filterFromLocalStorage || "All")
 
   return (
     <div className={styles.app}>
@@ -39,6 +31,7 @@ export default function App() {
         selectedFramework={selectedFramework}
       />
       */}
+      {selectedFramework}
       <Paginator />
     </div>
   );
