4b14ec671bbc475ac894a438340024b2ec9b64dd
Nacho <nkeiniger@gmail.com>
2022-01-28 11:12:01 -0300

    filter styles done

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Filter.module.css";
 
 enum FilterType {
-  All,
-  Favs,
+  All = "All",
+  MyFaves = "MyFaves",
 }
 
 export default function Filter() {
-  return <div>Filter</div>;
+  const [selectedButton, setSelectedButton] = useState("All");
+
+  return (
+    <div className={styles.button_container}>
+      <button
+        className={selectedButton === FilterType.All && styles.active}
+        onClick={() => setSelectedButton(FilterType.All)}
+      >
+        All
+      </button>
+      <button
+        className={selectedButton === FilterType.MyFaves && styles.active}
+        onClick={() => setSelectedButton(FilterType.MyFaves)}
+      >
+        My faves
+      </button>
+    </div>
+  );
 }
