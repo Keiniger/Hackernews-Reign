7309b85cca0ed97ba32977211262b9bee95bb38f
Nacho <nkeiniger@gmail.com>
2022-01-29 14:23:55 -0300

    paginator styled

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import dropdownIcon from "../assets/dropdown-icon.svg";
 import styles from "./Paginator.module.css";
 
 interface Props {
@@ -6,24 +7,27 @@ interface Props {
   setPage: (page: number) => void;
 }
 
-const visibleLength= 9;
+const visibleLength = 9;
 const minPage = 1;
 const maxPage = 49;
 
 const ascendingArray = (startsWith: number) => {
-  let array = Array.from({ length: visibleLength }, (_, i) => startsWith + i)
-  if(array[8]>maxPage) array = ascendingArray(maxPage-visibleLength+1)
+  let array = Array.from({ length: visibleLength }, (_, i) => startsWith + i);
+  if (array[8] > maxPage) array = ascendingArray(maxPage - visibleLength + 1);
   return array;
 };
 
 const descendingArray = (startsWith: number) => {
-  return Array.from({ length: visibleLength }, (_, i) => startsWith - i).reverse();
+  return Array.from(
+    { length: visibleLength },
+    (_, i) => startsWith - i
+  ).reverse();
 };
 
 export default function Paginator({ page, setPage }: Props) {
   const [visiblePages, setVisiblePages] = useState(ascendingArray(1));
-  const firstVisible = visiblePages[0],
-    lastVisible = visiblePages[8];
+  const firstVisible = visiblePages[0];
+  const lastVisible = visiblePages[8];
 
   const handlePageChange = (type: string) => {
     let newPage = 0;
@@ -43,26 +47,40 @@ export default function Paginator({ page, setPage }: Props) {
   };
 
   return (
-    <span style={{ display: "flex", flexDirection: "row" }}>
+    <ul className={styles.container}>
       <button
         disabled={page === minPage}
         onClick={() => handlePageChange("decrease")}
       >
-        {"<"}
+        <img
+          src={dropdownIcon}
+          className={`${styles.changePageIcon} ${styles.backIcon}`}
+          alt="back"
+        />
       </button>
-      {visiblePages.map((el) =>
-        el === page ? (
-          <button key={el} onClick={() => setPage(el)}>({el})</button>
-        ) : (
-          <button key={el} onClick={() => setPage(el)}>{el}</button>
-        )
-      )}
+      {visiblePages.map((el) => (
+        <li
+          className={el === page ? styles.currentPage : ""}
+          key={el}
+          onClick={() => setPage(el)}
+        >
+          {el}
+        </li>
+      ))}
       <button
         disabled={page === maxPage}
         onClick={() => handlePageChange("increase")}
       >
-        {">"}
+        <img
+          src={dropdownIcon}
+          className={`${styles.changePageIcon} ${styles.nextIcon}`}
+          alt="next"
+        />
       </button>
-    </span>
+    </ul>
   );
 }
+/*
+        disabled={page === minPage}
+        disabled={page === maxPage}
+*/
